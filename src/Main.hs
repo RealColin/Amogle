af870e0135d7ae3ba599bed98873918d9f6d553f
Werner, Colin <crwgond@gmail.com>
2025-04-01 16:50:23 -0400

    (WIP) Improve tokenization

--- src/Main.hs
+++ src/Main.hs
@@ -1,16 +1,34 @@
 import Text.Read (readMaybe)
+import Data.Char (isLower, isUpper, isAlpha)
 
 data Token =
   TokenLet
-  | TokenIdent String
+  | TokenTypeIdent String
+  | TokenValIdent String
   | TokenEquals
   | TokenInt Int
   | TokenDouble Double
   | TokenNL
+  | TokenIndent
   deriving (Show, Eq)
 
 tokenize :: String -> [Token]
-tokenize str = map getToken (words str)
+tokenize [] = []
+tokenize (' ':s) = tokenize s
+tokenize ('\t':s) = TokenIndent : tokenize s
+tokenize ('\n':s) = TokenNL : tokenize s
+tokenize (c:s)
+  | isAlpha c = getToken chunk : tokenize rest
+  | otherwise = TokenLet : tokenize s
+    where (chunk, rest) = takeChunk (c:s)
+
+takeChunk :: String -> (String, String)
+takeChunk [] = ([], [])
+takeChunk (c:s)
+  | isAlpha c = (c:chunk, rest)
+  | otherwise = ([], s)
+    where (chunk, rest) = takeChunk s
+    
 
 getToken :: String -> Token
 getToken "let" = TokenLet
@@ -18,11 +36,14 @@ getToken "=" = TokenEquals
 getToken str
   | Just n <- readMaybe str :: Maybe Int = TokenInt n
   | Just d <- readMaybe str :: Maybe Double = TokenDouble d
-  | otherwise = TokenIdent str
+  | otherwise = TokenValIdent str
 
 main :: IO ()
 main = do
   let x = tokenize "let x = 3"
   print x
-  let y = tokenize "let y = 3.0"
+  let y = tokenize "let y = 3\nlet z = 4"
   print y
+  let z = tokenize "letz = 3"
+  print z
+  
