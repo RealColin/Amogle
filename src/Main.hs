f5dac443a1ec6931e48886d68482a8dd0905c521
Werner, Colin <crwgond@gmail.com>
2025-04-01 12:06:05 -0400

    Add basic tokenizer

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,25 @@
+import Text.Read (readMaybe)
 
+data Token =
+  TokenLet
+  | TokenIdent String
+  | TokenEquals
+  | TokenInt Int
+  | TokenNL
+  deriving (Show, Eq)
 
+tokenize :: String -> [Token]
+tokenize str = map getToken (words str)
+
+getToken :: String -> Token
+getToken "let" = TokenLet
+getToken "=" = TokenEquals
+getToken str =
+  case readMaybe str :: Maybe Int of
+    Just n -> TokenInt n
+    Nothing -> TokenIdent str
+
+main :: IO ()
 main = do
-  print "Hello"
+  let x = tokenize "let x = 3"
+  print x
